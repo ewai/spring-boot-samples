1f9c40af7999ba30d54b4bec4c4389368dd567e3
ewai <ewai.info@gmail.com>
2017-07-12 19:40:03 +0900

    format

--- src/main/java/com/example/app/controller/SampleController.java
+++ src/main/java/com/example/app/controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SampleController {
 
-	@RequestMapping("sample")
+    @RequestMapping("sample")
     String loginForm() {
         return "sample";
     }
