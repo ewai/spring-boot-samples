88c4104b08f83fc856f27a9c78a7867cb0fe460d
ewai <ewai.info@gmail.com>
2017-08-01 17:20:18 +0900

    add repository test

--- src/test/java/com/example/samples/domain/ShouhinRepositoryTest.java
+++ src/test/java/com/example/samples/domain/ShouhinRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,31 +14,27 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.math.BigDecimal;
 
-import javax.persistence.EntityManager;
-
 @RunWith(SpringRunner.class)
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace=Replace.NONE)
+@AutoConfigureTestDatabase(replace=Replace.NONE)
 public class ShouhinRepositoryTest {
 
-//    @Autowired
-//  private TestEntityManager entityManager;
-//    private EntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Autowired
     private ShouhinRepository repository;
 
     @Test
     public void testExample() throws Exception {
-/*
+
         Shouhin testdata = new Shouhin();
-        testdata.setShouhinId("test1");
-        testdata.setShouhin("test2");
-        testdata.setShouhinVer(new BigDecimal(1729));
+        testdata.setShouhinId("00014939");
+        testdata.setShouhin("item name");
+        testdata.setShouhinVer(new BigDecimal(9999));
         this.entityManager.persist(testdata);
-*/
-        Shouhin shouhin = this.repository.findByShouhinIdAndShouhinVer("test1", new BigDecimal(1729));
-        assertThat("qqq").isEqualTo(shouhin.getShouhinId());
-        //assertThat(testdata.getShouhinId()).isEqualTo(shouhin.getShouhinId());
+
+        Shouhin shouhin = this.repository.findByShouhinIdAndShouhinVer("00014939", new BigDecimal(9999));
+        assertThat(testdata.getShouhinId()).isEqualTo(shouhin.getShouhinId());
     }
 }
